17365dae0a71a395e647d575acbaa2d34e8b0b4a
tarciorodrigo <tarciorodrigoa@gmail.com>
2025-06-04 14:19:45 -0300

    Implementation order buy and sell

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const crypto = require("crypto");
 const axios = require("axios");
 const db = require("./db");
+const { URLSearchParams } = require("url");
 
 const SYMBOL = "BTCUSDT";
-const BUY_PRICE = 104874;
-const SELL_PRICE = 105096;
+const BUY_PRICE = 105100;
+const SELL_PRICE = 105150;
+const QUANTITY = "0.001";
+
 const API_URL = "https://testnet.binance.vision";
 const API_URL_PROD = "https://api.binance.vision";
+
+const API_KEY = process.env.API_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
+//const API_KEY = "l2eAjEsMG8F7i90AjZWAHu4R4brmcwMxRLPc9BnNtWnMp16PDrBNDzacyKes5m0o";
+//const SECRET_KEY = "PYYHzy5uHcB4ML9HN6bR6Acnarnm6XTw8WRY6inOwtdci2j1hYZJ1utbIq2pPbDy";
 
 let isOpened = false;
 let operationName = "";
@@ -43,6 +52,7 @@ async function start() {
 
   if (price <= BUY_PRICE && !isOpened) {
     isOpened = true;
+    newOrder(SYMBOL, QUANTITY, "buy");
     console.log(`Buy at ${price}`);
 
     operationName = "BUY";
@@ -60,6 +70,7 @@ async function start() {
       .catch((err) => console.error("Error saving operation:", err));
   } else if (price >= SELL_PRICE && isOpened) {
     isOpened = false;
+    newOrder(SYMBOL, QUANTITY, "sell");
     console.log(`Sell at ${price}`);
 
     operationName = "SELL";
@@ -98,6 +109,7 @@ async function startSMA() {
 
   if (sma13 > sma21 && !isOpened) {
     isOpened = true;
+    newOrder(SYMBOL, QUANTITY, "buy");
     console.log(`Buy at ${price}`);
 
     operationName = "BUY";
@@ -115,6 +127,7 @@ async function startSMA() {
       .catch((err) => console.error("Error saving operation:", err));
   } else if (sma13 < sma21 && isOpened) {
     isOpened = false;
+    newOrder(SYMBOL, QUANTITY, "sell");
     console.log(`Sell at ${price}`);
 
     operationName = "SELL";
@@ -149,8 +162,9 @@ async function startSMA2() {
   console.log(`SMA (21): ${sma}`);
   console.log(`Is Opened: ${isOpened}`);
 
-  if (price <= sma * 0.9 && !isOpened) {
+  if (price <= sma * 0.9 && isOpened === false) {
     isOpened = true;
+    newOrder(SYMBOL, QUANTITY, "buy");
     console.log(`Buy at ${price}`);
 
     operationName = "BUY";
@@ -166,8 +180,9 @@ async function startSMA2() {
     db.insert(operation)
       .then(() => console.log("Operation saved to database"))
       .catch((err) => console.error("Error saving operation:", err));
-  } else if (price >= sma * 1.1 && isOpened) {
+  } else if (price >= sma * 1.1 && isOpened === true) {
     isOpened = false;
+    newOrder(SYMBOL, QUANTITY, "sell");
     console.log(`Sell at ${price}`);
 
     operationName = "SELL";
@@ -188,6 +203,37 @@ async function startSMA2() {
   console.log(`------------------------------`);
 }
 
-setInterval(startSMA, 3000);
+async function newOrder(symbol, quantity, side) {
+  const order = { symbol, quantity, side };
+  order.type = "MARKET";
+  order.timestamp = Date.now();
 
-startSMA();
+  const signature = crypto
+    .createHmac("sha256", SECRET_KEY)
+    .update(new URLSearchParams(order).toString())
+    .digest("hex");
+
+  order.signature = signature;
+
+  try {
+    const { data } = await axios.post(
+      `${API_URL}/api/v3/order`,
+      new URLSearchParams(order).toString(),
+      {
+        headers: {
+          "X-MBX-APIKEY": API_KEY,
+        },
+      }
+    );
+    console.log("Order response:", data);
+  } catch (err) {
+    console.error(
+      "Error placing order:",
+      err.response ? err.response.data : err.message
+    );
+  }
+}
+
+setInterval(start, 3000);
+
+start();
